2534e1a5f964d607ddc538dd221e36168283282c
Pepe Coral <coritochico@gmail.com>
2025-11-26 20:17:02 +0100

    fix: :bug: fix volume peek script

--- scripts/peek_volumes.sh
+++ scripts/peek_volumes.sh
@@ -3,6 +3,6 @@
 # This script opens a BusyBox container with files_volume and tmp_volume mounted.
 
 docker run --rm -it \
-  -v files_volume:/mnt/files \
-  -v tmp_volume:/mnt/tmp \
+  -v radiant-api_files_volume:/mnt/files \
+  -v radiant-api_tmp_volume:/mnt/tmp \
   busybox sh
