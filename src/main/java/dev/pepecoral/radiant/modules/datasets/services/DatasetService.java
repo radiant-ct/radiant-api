9536820d94d207aeb0e548b914740b06523c4525
Pepe Coral <coritochico@gmail.com>
2026-03-17 11:31:51 +0100

    feat: :white_check_mark: passed the creation  with null name test

--- src/main/java/dev/pepecoral/radiant/modules/datasets/services/DatasetService.java
+++ src/main/java/dev/pepecoral/radiant/modules/datasets/services/DatasetService.java
@@ -2,17 +2,20 @@ package dev.pepecoral.radiant.modules.datasets.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
 import dev.pepecoral.radiant.modules.datasets.entities.Dataset;
 import dev.pepecoral.radiant.modules.datasets.repositories.DatasetRepository;
+import jakarta.validation.Valid;
 
 @Service
+@Validated
 public class DatasetService {
 
     @Autowired
     DatasetRepository datasetRepository;
 
-    public Dataset create(Dataset dataset) {
+    public Dataset create(@Valid Dataset dataset) {
         return datasetRepository.save(dataset);
     }
 }
