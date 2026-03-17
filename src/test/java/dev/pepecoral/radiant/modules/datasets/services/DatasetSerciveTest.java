4c7b5b67a4024af9db2e00955b8eff9bf730902d
Pepe Coral <coritochico@gmail.com>
2026-03-17 11:54:35 +0100

    test: :test_tube: added a failing test for get dataset by id

--- src/test/java/dev/pepecoral/radiant/modules/datasets/services/DatasetSerciveTest.java
+++ src/test/java/dev/pepecoral/radiant/modules/datasets/services/DatasetSerciveTest.java
@@ -1,5 +1,6 @@
 package dev.pepecoral.radiant.modules.datasets.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import dev.pepecoral.radiant.modules.common.builders.TestPersistenceContext;
 import dev.pepecoral.radiant.modules.datasets.builders.DatasetTestBuilder;
 import dev.pepecoral.radiant.modules.datasets.entities.Dataset;
 import jakarta.validation.ConstraintViolationException;
@@ -18,6 +20,9 @@ public class DatasetSerciveTest {
 
     @Autowired
     DatasetService datasetService;
+
+    @Autowired
+    TestPersistenceContext testPersistenceContext;
 
     @Test
     public void shouldCreateDataset_whenValidData() {
@@ -67,6 +72,17 @@ public class DatasetSerciveTest {
     public void shouldThrow_whenCreateDatasetWithIdNotNull() {
         Dataset dataset = DatasetTestBuilder.builder().id(UUID.randomUUID()).build().entity();
         assertThrows(IllegalArgumentException.class, () -> datasetService.create(dataset));
+    }
+
+    @Test
+    public void shouldGetDatasetById() {
+        Dataset dataset = DatasetTestBuilder.builder().build().persist(testPersistenceContext);
+        Dataset gotDataset = datasetService.getById(dataset.getId());
+
+        assertNotNull(gotDataset);
+        assertEquals(dataset, gotDataset);
+        assertEquals(dataset.getName(), gotDataset.getName());
+        assertEquals(dataset.getDescription(), gotDataset.getDescription());
     }
 
 }
