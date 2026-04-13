ba4f83894f2bf2a92b69f104825af9a94c3f4b35
Pepe Coral <coritochico@gmail.com>
2026-04-13 09:25:44 +0200

    test: :test_tube: added a failing test for getting a dataset

---
+++ src/test/java/dev/pepecoral/radiant/modules/datasets/controllers/DatasetControllerTest.java
@@ -0,0 +1,39 @@
+package dev.pepecoral.radiant.modules.datasets.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import dev.pepecoral.radiant.modules.datasets.builders.DatasetTestBuilder;
+import dev.pepecoral.radiant.modules.datasets.entities.Dataset;
+import dev.pepecoral.radiant.modules.datasets.services.DatasetService;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(DatasetController.class)
+public class DatasetControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockitoBean
+    DatasetService datasetService;
+
+    @Test
+    public void datasetController_shouldReturnDataset_whenAskedById() throws Exception {
+
+        Dataset createdDataset = DatasetTestBuilder.builder().build().entity();
+
+        when(datasetService.findById(createdDataset.getId()))
+                .thenReturn(createdDataset);
+
+        mockMvc.perform(get("/datasets/" + createdDataset.getId()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.id").value(createdDataset.getId()));
+    }
+}
