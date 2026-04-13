20f743d06d988adb008ce85a4da747e5baa877f7
Pepe Coral <coritochico@gmail.com>
2026-04-13 09:02:40 +0200

    test: :test_tube: added a failing test for the health endpoint

---
+++ src/test/java/dev/pepecoral/radiant/modules/health/controllers/HealthControllerTest.java
@@ -0,0 +1,24 @@
+package dev.pepecoral.radiant.modules.health.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HealthController.class)
+class HealthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void healthController_shouldAnswer() throws Exception {
+        mockMvc.perform(get("/health"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Server is up"));
+    }
+}
