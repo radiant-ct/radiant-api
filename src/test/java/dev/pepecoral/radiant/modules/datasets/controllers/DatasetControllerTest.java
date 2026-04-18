c0ded30a1e706ff5df133f1893e601622dc2be69
Pepe Coral <coritochico@gmail.com>
2026-04-18 12:10:54 +0200

    test: :test_tube: added a test for get all images in a dataset endpoint

--- src/test/java/dev/pepecoral/radiant/modules/datasets/controllers/DatasetControllerTest.java
+++ src/test/java/dev/pepecoral/radiant/modules/datasets/controllers/DatasetControllerTest.java
@@ -7,35 +7,72 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import dev.pepecoral.radiant.modules.datasets.builders.DatasetTestBuilder;
+import dev.pepecoral.radiant.modules.datasets.builders.ImageTestBuilder;
 import dev.pepecoral.radiant.modules.datasets.entities.Dataset;
+import dev.pepecoral.radiant.modules.datasets.entities.Image;
 import dev.pepecoral.radiant.modules.datasets.services.DatasetService;
+import dev.pepecoral.radiant.modules.datasets.services.ImageService;
 
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.IntStream;
 
 @WebMvcTest(DatasetController.class)
 public class DatasetControllerTest {
 
-    @Autowired
-    MockMvc mockMvc;
+        @Autowired
+        MockMvc mockMvc;
 
-    @MockitoBean
-    DatasetService datasetService;
+        @MockitoBean
+        DatasetService datasetService;
 
-    @Test
-    public void datasetController_shouldReturnDataset_whenAskedById() throws Exception {
+        @MockitoBean
+        ImageService imageService;
 
-        Dataset createdDataset = DatasetTestBuilder.builder().id(UUID.randomUUID()).build().entity();
+        @Test
+        public void datasetController_shouldReturnDataset_whenAskedById() throws Exception {
 
-        when(datasetService.findById(createdDataset.getId()))
-                .thenReturn(createdDataset);
+                Dataset createdDataset = DatasetTestBuilder.builder().id(UUID.randomUUID()).build().entity();
 
-        mockMvc.perform(get("/datasets/" + createdDataset.getId()))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"))
-                .andExpect(jsonPath("$.id").value(createdDataset.getId().toString()));
-    }
+                when(datasetService.findById(createdDataset.getId()))
+                                .thenReturn(createdDataset);
+
+                mockMvc.perform(get("/datasets/" + createdDataset.getId()))
+                                .andExpect(status().isOk())
+                                .andExpect(content().contentType("application/json"))
+                                .andExpect(jsonPath("$.id").value(createdDataset.getId().toString()));
+        }
+
+        @Test
+        public void datasetController_shouldReturnImages_whenAskedById() throws Exception {
+
+                Dataset dataset = DatasetTestBuilder.builder().id(UUID.randomUUID()).build().entity();
+
+                List<Image> images = IntStream.range(0, 5)
+                                .mapToObj(i -> ImageTestBuilder.builder()
+                                                .dataset(dataset)
+                                                .id(UUID.randomUUID())
+                                                .build()
+                                                .entity())
+                                .toList();
+
+                when(datasetService.findById(dataset.getId()))
+                                .thenReturn(dataset);
+
+                when(imageService.findByDataset(dataset)).thenReturn(images);
+
+                mockMvc.perform(get("/datasets/" + dataset.getId() + "/images"))
+                                .andExpect(status().isOk())
+                                .andExpect(content().contentType("application/json"))
+                                .andExpect(jsonPath("$.length()").value(5));
+
+                verify(datasetService).findById(dataset.getId());
+                verify(imageService).findByDataset(dataset);
+        }
+
 }
