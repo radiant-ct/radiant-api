e3b284efdf05f6e53ff9f690fcc2db2ad0d5c7d9
Pepe Coral <coritochico@gmail.com>
2026-05-17 23:46:48 +0200

    feat: :sparkles: added a configuration object for file storage config

--- src/main/java/dev/pepecoral/radiant/modules/common/config/FileStorageProperties.java
+++ src/main/java/dev/pepecoral/radiant/modules/common/config/FileStorageProperties.java
@@ -3,17 +3,15 @@ package dev.pepecoral.radiant.modules.common.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Component
 @ConfigurationProperties(prefix = "file")
+@Getter
+@Setter
 public class FileStorageProperties {
 
     private String queueDir = "/queue";
 
-    public String getQueueDir() {
-        return queueDir;
-    }
-
-    public void setQueueDir(String uploadDir) {
-        this.queueDir = uploadDir;
-    }
 }
